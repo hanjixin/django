c066c572e425db43d4b58b25f10d94ab8a797f48
hank <764671440@qq.com>
2019-08-02 18:51:00 +0800

    init

---
+++ testdj/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def hello(request):
+    context          = {}
+    context['name'] = 'Hello World!'
+    context['description'] = 'Hello World!'
+    return render(request, 'index.html', )
